3f635b56563b9d82e3f0a98ff5ed515d19922fc0
AdarshKanaujiya <adarkanjiya@gmail.com>
2024-09-22 14:12:01 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'app';
+  stringToBeEvaluated:string="";
+  takeInput(num:string){
+    this.stringToBeEvaluated = this.stringToBeEvaluated + num;
+    //console.log(this.stringToBeEvaluated);
+  }
+
+  evaluateString(){
+    if (this.stringToBeEvaluated!=""){
+      this.stringToBeEvaluated = eval(this.stringToBeEvaluated);
+    }
+    //console.log(this.stringToBeEvaluated);
+  }
+
+  clearString(){
+    this.stringToBeEvaluated = "";
+  }
+
 }
